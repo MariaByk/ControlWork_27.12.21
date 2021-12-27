653047cfd7e224fb134ec42d720cea76c4227cfb
LabuzovaMariaya <myubykova@yandex.ru>
2021-12-27 21:33:48 +0300

    Task is solved

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Array(int[] collection)
+{
+    int length = collection.Length;
+    int i = 0;
+    int index = 0;
+    for (index = 0;index < length;index++)
+    {
+        collection[index] = new Random().Next(1,10);       
+        if (collection[index] % 2 == 0) i++;
+    }
+    PrintArray(collection);
+    Console.WriteLine();
+    int[] newArray = new int[i];
+    index = 0;
+    int ind = 0;
+    while (index < length)
+    {
+        if (collection[index] % 2 == 0)
+        {
+            newArray[ind] = collection[index];
+            ind++;
+          
+        }
+        index++;
+    }
+ PrintArray(newArray);   
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    
+    for (int position = 0; position < count;position++)
+    {
+        Console.WriteLine(col[position]);
+    }
+}
+
+
+int[] array = new int[6];
+Array(array);
+
